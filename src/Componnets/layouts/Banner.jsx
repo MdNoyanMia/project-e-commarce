7718086e87dbb2c421f0591c8bfcdb8be54643a0
MdNoyanMia <mdnoyanmia22@gmail.com>
2025-08-13 23:53:50 +0600

    four line

--- src/Componnets/layouts/Banner.jsx
+++ src/Componnets/layouts/Banner.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
-import Container from '../Container'
+
 
 const Banner = () => {
   return (
     <>
-    <Container>
-        <div  class="bg-[url(/src/assets/bbenner.png)] h-[500px] bg-no-repeat bg-center bg-cover">
-        </div>
-    </Container>
+
+      <div class="bg-[url(/src/assets/bbenner.png)] h-[500px] bg-no-repeat bg-center bg-cover">
+      </div>
+
     </>
   )
 }
--- src/Componnets/layouts/Prouductiteam.jsx
+++ src/Componnets/layouts/Prouductiteam.jsx
@@ -3,16 +3,33 @@ import Badge from '../Badge'
 import Container from '../Container'
 import Image from '../Image'
 import picture1 from '/src/assets/picture1.png'
+import { Link } from 'react-router-dom'
+import { FaHeart } from "react-icons/fa";
+import { TbRefresh } from "react-icons/tb";
+import { FaCartShopping } from "react-icons/fa6";
+import Heading from '../Heading'
 
 const Prouductiteam = () => {
     return (
         <>
-            <div className='w-1/4 relative'>
+            <div className='w-1/4 relative group'>
                 <Container>
-                 
-                        <Badge badgeText={'new'} className='absolute top-3.5 left-3.5'/>
-                        <Image imgSrc={picture1}/>
-                 
+                    <Image imgSrc={picture1} />
+                    <Badge badgeText={'New'} className='absolute top-6 left-6' />
+
+                    <div className='bg-amber-300 py-[25px] px-[30px] absolute bottom-0 left-0 w-full opacity-0 group-hover:opacity-100 duration-700'>
+
+
+                        <Link to={'/'} className='flex items-center justify-end gap-x-2'>Add to Wish List<FaHeart /></Link>
+
+                        <Link to={'/'} className='flex items-center justify-end gap-x-2'>Compare<TbRefresh /></Link>
+
+                        <Link to={'/'} className='flex items-center justify-end gap-x-2'> Add to Cart<FaCartShopping /></Link>
+                    </div>
+
+                    <div className='mt-2 bg-amber-900'>
+                        <Heading Text={" Basic Crew Neck Tee"} as={"h3"}/>
+                    </div>
                 </Container>
             </div>
         </>
